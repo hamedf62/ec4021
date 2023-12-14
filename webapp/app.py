97868c827b14314a19ddcf33ae26e8e80a2b662c
Hamed Fallah <hamedf@gmail.com>
2023-12-14 18:25:02 +0330

    flask + sqlalchemy for database

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,32 @@
 from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker
 
+engine = create_engine("sqlite:///flask_db.db", echo=False)
+
+Base = declarative_base()
+
+
+class Contact(Base):
+    __tablename__ = "contact"
+    id = Column(Integer, primary_key=True)
+    fname = Column(String)
+    email = Column(String)
+    description = Column(String)
+
+
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# add test record to database
+# session.add(Contact(fname="hasan", email="hasan@yahoo.com", description="nothing"))
+# session.commit()
+
+
+# create the flask app
 app = Flask(__name__)
 
 
@@ -20,21 +47,17 @@ def contact_us():
     show_form = False
     fname = ""
     if len(request.args) == 0:
-        # pass
         show_form = True
 
     else:
-        # pass
         fname = request.args["fname"]
+        email = request.args["email"]
+        description = request.args["description"]
+
+        session.add(Contact(fname=fname, email=email, description=description))
+        session.commit()
+
     return render_template("contact.html", show_form=show_form, fname=fname)
-
-
-# @app.route("/result", methods=["GET"])
-# def contact_result():
-#     fname = request.args["fname"]
-#     email = request.args["email"]
-#     description = request.args["description"]
-#     return request.args
 
 
 if __name__ == "__main__":
